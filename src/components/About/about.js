e782a2484ffb8c31dd3fa61df1bf000cc0e3642e
kyle-mucerino <kyle.mucerino@gmail.com>
2024-09-11 14:47:04 -0700

    Initial reconfiguration

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,6 +3,16 @@ import "./about.css";
 import Image1 from "../../assets/AOTopQs.png";
 import Image2 from "../../assets/homegif.gif";
 import Image4 from "../../assets/AOChatBot.png";
+import gif from "../../assets/homegif.gif";
+import gif2 from "../../assets/school.gif";
+import gif3 from "../../assets/students.gif";
+import gif4 from "../../assets/education.gif";
+import gif5 from "../../assets/secretary.gif";
+import gif6 from "../../assets/bell.gif";
+import gif7 from "../../assets/lesson.gif";
+import gif8 from "../../assets/teacher.gif";
+import gif9 from "../../assets/blackboard.gif";
+import gif10 from "../../assets/teacher2.gif";
 
 function About() {
   const [activeImage, setActiveImage] = useState("image1");
@@ -110,6 +120,169 @@ function About() {
       </div>
 
       {/* SECTION 3 */}
+      <div className="section section-3">
+        <div className="ag-format-container p-4">
+          <div className="ag-courses_box saira-regular hidden custom:grid grid-cols-8">
+            <div className="ag-courses_item col-start-1 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">School-Friendly AI</div>
+
+                <div className="ag-courses-item_date-box">
+                  Our AI is trained to be an expert in school questions and
+                  topics right out of the box. <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+            <div className="col-start-5 col-span-1 my-auto">
+              <img src={gif2} alt="school gif" className="rounded-3xl" />
+            </div>
+            <div className="col-start-7 col-span-1 my-auto">
+              <img src={gif3} alt="school gif" className="rounded-3xl" />
+            </div>
+            <div className="ag-courses_item col-start-2 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">
+                  Answer Quality Tools
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  Custom FAQs (not required), AI website training, and answer
+                  feedback help improve chatbot answers. <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+            <div className="col-start-6 col-span-1 my-auto">
+              <img src={gif7} alt="school gif" className="rounded-3xl" />
+            </div>
+            <div className="col-start-8 col-span-1 my-auto">
+              <img src={gif4} alt="school gif" className="rounded-3xl" />
+            </div>
+
+            {/* <div className="col-start-2 col-span-1 my-auto">
+      <i className="fa-solid fa-bolt text-9xl">
+        <span className="sr-only">Lightning Bolt</span>
+      </i>
+    </div> */}
+
+            <div className="ag-courses_item col-start-3 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">Contact Us Routing</div>
+
+                <div className="ag-courses-item_date-box">
+                  Route questions to specific staff members (if person is
+                  needed) based on topic. <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+            <div className="col-start-7 col-span-1 my-auto">
+              <img src={gif6} alt="school gif" className="rounded-3xl" />
+            </div>
+
+            {/* <div className="col-start-1 col-span-1 my-auto">
+      <img src={gif7} alt="school gif" className="rounded-3xl" />
+    </div> */}
+            <div className="col-start-2 col-span-1 my-auto">
+              <img src={gif8} alt="school gif" className="rounded-3xl" />
+            </div>
+
+            <div className="ag-courses_item col-start-4 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">
+                  Language Translation
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  English, Spanish, and 10 other languages available. More
+                  coming soon! <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+            {/* <div className="col-start-7 col-span-1 my-auto justify-self-end">
+      <i className="fa-solid fa-bolt text-9xl">
+        <span className="sr-only">Lightning Bolt</span>
+      </i>
+    </div> */}
+
+            <div className="col-start-1 col-span-1 my-auto">
+              <img src={gif9} alt="school gif" className="rounded-3xl" />
+            </div>
+            <div className="col-start-3 col-span-1 my-auto">
+              <img src={gif10} alt="school gif" className="rounded-3xl" />
+            </div>
+
+            <div className="ag-courses_item col-start-5 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">Analytics</div>
+
+                <div className="ag-courses-item_date-box">
+                  Question level data that gives you a real time passive focus
+                  group and measurement of stakeholder needs. <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+
+            <div className="col-start-2 col-span-1 my-auto">
+              <img src={gif2} alt="school gif" className="rounded-3xl" />
+            </div>
+            <div className="col-start-4 col-span-1 my-auto">
+              <img src={gif4} alt="school gif" className="rounded-3xl" />
+            </div>
+
+            <div className="ag-courses_item col-start-6 col-span-3">
+              <a href="/features" className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">
+                  Use Multiple Chatbots
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  Districts can build unlimited chatbots, from district-wide to
+                  school site or topic specific. <br />
+                  <button className="btn info mt-4 font-black">
+                    Learn More&rarr;
+                  </button>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* GIF SPEECH BUBBLE */}
+      <div className="flex flex-row justify-center my-8">
+        <div>
+          <img src={gif} alt="gif" className="gif rounded-lg" />
+        </div>
+        <p className="bubble left alata-regular text-2xl w-1/5 self-center">
+          See the Chat Bot in action!
+        </p>
+      </div>
     </>
   );
 }
--- src/components/Platform/platform.js
+++ src/components/Platform/platform.js
@@ -11,6 +11,7 @@ import gif7 from "../../assets/lesson.gif";
 import gif8 from "../../assets/teacher.gif";
 import gif9 from "../../assets/blackboard.gif";
 import gif10 from "../../assets/teacher2.gif";
+import chatBot from "../../assets/AOChatBot.png";
 
 
 function Platform() {
@@ -62,40 +63,31 @@ function Platform() {
   return (
     <>
       {/* SECTION 1 */}
-      <div className="section section-1 flex flex-col">
-        <div className="flex flex-col mx-auto justify-evenly text-center mt-8">
+      <div className="section section-1 flex flex-row">
+        <div className="flex flex-col justify-evenly text-left ml-12">
           <div className="headline mb-4">
-            <h1 className="text-7xl alata-regular">
+            <h1 className="text-8xl alata-regular">
               AlwaysOn{" "}
-              <i className="fa-solid fa-bolt">
+              {/* <i className="fa-solid fa-bolt">
                 <span className="sr-only">Lightning Bolt</span>
-              </i>
+              </i> */}
             </h1>
           </div>
           <div>
-            <p className="text-2xl saira-medium">
-              AI powered customer service platform for schools & districts
+            <p className="text-4xl saira-medium">
+              A powerful AI chatbot in the center of a customer service platform
             </p>
           </div>
-          <div className="free-trial my-6">
-            <button className="border-2 border-white bg-white text-black rounded-lg p-4 freeman-regular uppercase mx-4">
+          <div className="free-trial">
+            <button className="border-2 border-white bg-white text-black rounded-lg p-6 freeman-regular uppercase">
               <span>Get started today</span>
             </button>
           </div>
-          <div className="mb-4">
-            <p className="alata-regular text-3xl">
-              100+ schools & districts in 13 states enhance their schools online
-              presence with AlwaysOn
-            </p>
-          </div>
         </div>
-        <div className="flex flex-row justify-center my-8">
+        <div className="flex flex-row justify-center items-center">
           <div>
-            <img src={gif} alt="gif" className="gif rounded-lg" />
+            <img src={chatBot} alt="gif" className="w-1/2 rounded-lg mx-auto" />
           </div>
-          <p className="bubble left alata-regular text-2xl w-1/5 self-center">
-            See the Chat Bot in action!
-          </p>
         </div>
       </div>
 
@@ -132,160 +124,6 @@ function Platform() {
       </div>
 
       {/* SECTION 3 */}
-
-      <div className="section section-3">
-        <div className="ag-format-container p-4">
-          <div className="ag-courses_box saira-regular hidden custom:grid grid-cols-8">
-            <div className="ag-courses_item col-start-1 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">School-Friendly AI</div>
-
-                <div className="ag-courses-item_date-box">
-                  Our AI is trained to be an expert in school questions and
-                  topics right out of the box. <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-            <div className="col-start-5 col-span-1 my-auto">
-              <img src={gif2} alt="school gif" className="rounded-3xl" />
-            </div>
-            <div className="col-start-7 col-span-1 my-auto">
-              <img src={gif3} alt="school gif" className="rounded-3xl" />
-            </div>
-            <div className="ag-courses_item col-start-2 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">
-                  Answer Quality Tools
-                </div>
-
-                <div className="ag-courses-item_date-box">
-                  Custom FAQs (not required), AI website training, and answer
-                  feedback help improve chatbot answers. <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-            <div className="col-start-6 col-span-1 my-auto">
-              <img src={gif7} alt="school gif" className="rounded-3xl" />
-            </div>
-            <div className="col-start-8 col-span-1 my-auto">
-              <img src={gif4} alt="school gif" className="rounded-3xl" />
-            </div>
-
-            {/* <div className="col-start-2 col-span-1 my-auto">
-      <i className="fa-solid fa-bolt text-9xl">
-        <span className="sr-only">Lightning Bolt</span>
-      </i>
-    </div> */}
-
-            <div className="ag-courses_item col-start-3 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">Contact Us Routing</div>
-
-                <div className="ag-courses-item_date-box">
-                  Route questions to specific staff members (if person is
-                  needed) based on topic. <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-            <div className="col-start-7 col-span-1 my-auto">
-              <img src={gif6} alt="school gif" className="rounded-3xl" />
-            </div>
-
-            {/* <div className="col-start-1 col-span-1 my-auto">
-      <img src={gif7} alt="school gif" className="rounded-3xl" />
-    </div> */}
-            <div className="col-start-2 col-span-1 my-auto">
-              <img src={gif8} alt="school gif" className="rounded-3xl" />
-            </div>
-
-            <div className="ag-courses_item col-start-4 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">
-                  Language Translation
-                </div>
-
-                <div className="ag-courses-item_date-box">
-                  English, Spanish, and 10 other languages available. More
-                  coming soon! <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-            {/* <div className="col-start-7 col-span-1 my-auto justify-self-end">
-      <i className="fa-solid fa-bolt text-9xl">
-        <span className="sr-only">Lightning Bolt</span>
-      </i>
-    </div> */}
-
-            <div className="col-start-1 col-span-1 my-auto">
-              <img src={gif9} alt="school gif" className="rounded-3xl" />
-            </div>
-            <div className="col-start-3 col-span-1 my-auto">
-              <img src={gif10} alt="school gif" className="rounded-3xl" />
-            </div>
-
-            <div className="ag-courses_item col-start-5 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">Analytics</div>
-
-                <div className="ag-courses-item_date-box">
-                  Question level data that gives you a real time passive focus
-                  group and measurement of stakeholder needs. <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-
-            <div className="col-start-2 col-span-1 my-auto">
-              <img src={gif2} alt="school gif" className="rounded-3xl" />
-            </div>
-            <div className="col-start-4 col-span-1 my-auto">
-              <img src={gif4} alt="school gif" className="rounded-3xl" />
-            </div>
-
-            <div className="ag-courses_item col-start-6 col-span-3">
-              <a href="/features" className="ag-courses-item_link">
-                <div className="ag-courses-item_bg"></div>
-
-                <div className="ag-courses-item_title">
-                  Use Multiple Chatbots
-                </div>
-
-                <div className="ag-courses-item_date-box">
-                  Districts can build unlimited chatbots, from district-wide to
-                  school site or topic specific. <br />
-                  <button className="btn info mt-4 font-black">
-                    Learn More&rarr;
-                  </button>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
 
       {/* SECTION 4 */}
       <div className="section section-4 hidden custom:flex flex-col">
