151f6148a088d7c989fb94f42dcf4ef70ae5ec0c
kyle-mucerino <kyle.mucerino@gmail.com>
2024-12-09 12:55:43 -0800

    colorway for reveal cards

--- src/components/CanvasRevealEffectDemo.js.js
+++ src/components/CanvasRevealEffectDemo.js.js
@@ -6,30 +6,26 @@ import { CanvasRevealEffect } from "./CanvasRevealEffect";
 export function CanvasRevealEffectDemo() {
   return (
     <>
-      <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8 cursor-pointer">
+      <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8 cursor-pointer open-sans-light">
         <Card
-          title="ChatBot"
+          title="CHATBOT"
           info="Turnkey: Works instantly no setup required) Custom: Trained only on your website information) Translation: Communicate in multiple languages) Training: Improvement with every interaction) Multi-Chatbot: District-wide, school site, dept, etc."
         >
           <CanvasRevealEffect
             animationSpeed={3}
-            containerClassName="bg-black"
-            colors={[
-              [236, 72, 153],
-              [232, 121, 249]
-            ]}
-            dotSize={2}
+            containerClassName="bg-orange-500"
+            colors={([153, 51, 255], [0, 255, 85])}
           />
         </Card>
         <button class="text-8xl font-bold text-white">&#43;</button>
         <Card
-          title="Analytics"
+          title="ANALYTICS"
           info="Usage Metrics: Track the number of unique engagements and overall volume of questions) Question-Level Data: See the exact questions and specific information your community requests) Predictive Insights: Identify information gaps, trends, and anticipate needs to improve the website"
         >
           <CanvasRevealEffect
             animationSpeed={3}
-            containerClassName="bg-sky-600"
-            colors={[[125, 211, 252]]}
+            containerClassName="bg-orange-500"
+            colors={([153, 51, 255], [0, 255, 85])}
           />
         </Card>
       </div>
@@ -47,7 +43,7 @@ const Card = ({ title, info, children }) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="relative border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 h-[30rem] hover:shadow-xl transition duration-300 ease-in-out"
+      className="relative border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-[36rem] w-full mx-auto p-4 h-[32rem] hover:shadow-xl transition duration-300 ease-in-out"
     >
       <AnimatePresence>
         {hovered && (
@@ -62,7 +58,7 @@ const Card = ({ title, info, children }) => {
       </AnimatePresence>
       <div className="relative z-20">
         {/* Title */}
-        <div className="text-center transition duration-200 w-full mx-auto flex items-center justify-center mb-4">
+        <div className="text-center transition duration-200 w-full mx-auto flex items-center justify-center mb-8">
           <h2 className="dark:text-white text-5xl relative z-10 text-black font-bold">
             {title}
           </h2>
@@ -74,16 +70,22 @@ const Card = ({ title, info, children }) => {
             hovered ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-4"
           }`}
         >
-          <ul className="list-disc list-inside dark:text-white text-black text-md text-left space-y-2 text-xl">
-            {infoItems.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
+          <ul className="list-none list-inside dark:text-white text-black text-2xl text-left space-y-4">
+            {infoItems.map((item, index) => {
+              const [boldText, rest] = item.split(":");
+              return (
+                <li key={index}>
+                  <span className="font-bold">{boldText}:</span>
+                  {rest && ` ${rest}`}
+                </li>
+              );
+            })}
           </ul>
         </div>
 
         {/* Hover Prompt */}
         {!hovered && (
-          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-md text-gray-500 dark:text-gray-400 animate-bounce">
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-lg text-gray-500 dark:text-gray-400 animate-bounce uppercase">
             Hover for more details
           </div>
         )}
