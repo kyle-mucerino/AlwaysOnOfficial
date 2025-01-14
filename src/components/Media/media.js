b4e4c41d602a11556c6cf7221e7d0992a70a8a26
kyle-mucerino <kyle.mucerino@gmail.com>
2025-01-14 14:20:06 -0800

    downloadable media page items

--- src/components/Media/media.js
+++ src/components/Media/media.js
@@ -1,50 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import "./media.css";
-import onePager from "../../assets/onePager.pdf";
-import onePagerImg from "../../assets/onePager.png";
 
 function Media() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Function to toggle modal visibility
-  const toggleModal = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
-      <div className={isOpen ? "blur-background" : ""}>
+      <div>
         {/* Sections */}
         <section className="documents border-t-2 border-black p-4">
           <div className="flex flex-col space-y-4 mt-4">
             <h1 className="text-2xl sm:text-5xl poppins-bold">Documents</h1>
             <ul className="text-sm sm:text-2xl open-sans-light list-none space-y-4">
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1hibn-C-CKZyNMoVd8tVoU2iJ021519QZ"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download AlwaysOn Company & Product One Pager"
                 >
                   AlwaysOn Company & Product One Pager
-                </button>
+                </a>
               </li>
+              <li>AlwaysOn Company & Product Presentation</li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
-                >
-                  AlwaysOn Company & Product Presentation
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=14Ge_HYk8Jx4wok_TReOIHD1zBkLazZBe"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download AlwaysOn Voluntary Product Accessibility Template (VPAT)"
                 >
                   AlwaysOn Voluntary Product Accessibility Template (VPAT)
-                </button>
+                </a>
               </li>
             </ul>
           </div>
@@ -59,56 +44,60 @@ function Media() {
                   href="https://www.loom.com/share/ad49b8627f174d23a59a8449dec2120b?sid=0a334d1b-2722-4e7f-bd16-b9dc61711e6e"
                   target="_blank"
                   rel="noreferrer"
-                  aria-label="open one pager"
-                  className=""
+                  aria-label="download open video overview"
                 >
                   AlwaysOn Company & Product Overview
                 </a>
               </li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1oxgBV4jG-leG0OSpSJEDez418ejrJM8Q"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download Website Chatbot + Analytics informational video"
                 >
                   Website Chatbot + Analytics
-                </button>
+                </a>
               </li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1Hk2Q0g1xo8e0t1r0ZY1zh_p1WgXHbKiY"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download Document Chabot + Analytics informational video"
                 >
                   Document Chatbot + Analytics
-                </button>
+                </a>
               </li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1zDmETSHHrRfpU-zbv0Hq4NaJFEro6BGJ"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download Survey Tool informational video"
                 >
                   Survey Tool
-                </button>
+                </a>
               </li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1oxgBV4jG-leG0OSpSJEDez418ejrJM8Q"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download Conact Us Routing informational video"
                 >
                   Contact Us Routing
-                </button>
+                </a>
               </li>
               <li>
-                <button
-                  onClick={toggleModal}
-                  aria-label="open one pager"
-                  className=""
+                <a
+                  href="https://drive.google.com/uc?export=download&id=1G2LmHjtjNv7ugg_naD5oieX8Z-fO1Rlv"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="download Prefessional Services informational video"
                 >
                   Professional Services
-                </button>
+                </a>
               </li>
             </ul>
           </div>
@@ -121,10 +110,9 @@ function Media() {
               <li>
                 <a
                   href="https://podcasts.apple.com/us/podcast/mastering-social-media-for-schools/id1509779834?i=1000655391023"
-                  aria-label="open one pager"
+                  aria-label="listen to podcast"
                   target="_blank"
                   rel="noreferrer"
-                  className=""
                 >
                   Mastering Social Media for Schools: Using AI to Improve School
                   Customer Service with Teddy Daiber
@@ -141,10 +129,9 @@ function Media() {
               <li>
                 <a
                   href="https://apptegy.wistia.com/medias/qrkk5sjpco"
-                  aria-label="open one pager"
+                  aria-label="watch webinar"
                   target="_blank"
                   rel="noreferrer"
-                  className=""
                 >
                   AlwaysOn AI Chatbot with Apptegy Websites
                 </a>
@@ -160,10 +147,9 @@ function Media() {
               <li>
                 <a
                   href="https://www.greenwichtime.com/news/education/article/greenwich-sleuthhound-chatbot-schools-19988778.php"
-                  aria-label="open one pager"
+                  aria-label="read article"
                   target="_blank"
                   rel="noreferrer"
-                  className=""
                 >
                   Greenwich Time: "Meet Greenwich Public Schools new chatbot who
                   won't say why the district got rid of tacos at lunch"
@@ -172,10 +158,9 @@ function Media() {
               <li>
                 <a
                   href="https://patch.com/pennsylvania/warminster/ai-chatbot-pilot-launched-centennial-website"
-                  aria-label="open one pager"
+                  aria-label="read article"
                   target="_blank"
                   rel="noreferrer"
-                  className=""
                 >
                   Patch: “AI Chatbot Pilot Launched On Centennial Website”
                 </a>
@@ -183,10 +168,9 @@ function Media() {
               <li>
                 <a
                   href="https://www.psusd.us/site/handlers/filedownload.ashx?moduleinstanceid=18882&dataid=20975&FileName=WEB_OctNov%202021%20English.pdf"
-                  aria-label="open one pager"
+                  aria-label="read article"
                   target="_blank"
                   rel="noreferrer"
-                  className=""
                 >
                   Palm Springs: “PSUSD Website Has New Chat Option for Quick,
                   Easy Responses”
@@ -196,18 +180,6 @@ function Media() {
           </div>
         </section>
       </div>
-
-      {/* Modal */}
-      {isOpen && (
-        <div className="pdf-modal">
-          <div className="pdf-modal-content">
-            <span className="close" onClick={toggleModal}>
-              &times;
-            </span>
-            <iframe src={onePager} width="100%" title="PDF Viewer"></iframe>
-          </div>
-        </div>
-      )}
     </>
   );
 }
