8a7e10a4029d18d3591129fd834b7f704fdd8d44
kyle-mucerino <kyle.mucerino@gmail.com>
2025-01-27 11:09:39 -0800

    login page style

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,22 +1,23 @@
-import '../Login/login.css';
+import "../Login/login.css";
 
 const Login = () => {
   return (
-    <div className="login justify-center items-center flex flex-col border-top-2 border-black border-2">
-      <h1 className="text-3xl sm:text-6xl poppins-bold p-2 sm:p-4 mt-2">
-        Login to the Platform
+    <div className="login flex flex-col justify-center items-center p-6 border-top border-black border-2">
+      <h1 className="text-3xl sm:text-6xl font-bold text-center">
+        Login to AlwaysOn's Platform
       </h1>
+      <p className="text-3xl sm:text-6xl my-2">&darr;</p>
       <a
-        className="text-lg sm:text-4xl open-sans-regular p-2 sm:p-2 border-black border-2 rounded-lg bg-yellow-500"
+        className="px-8 py-4 text-lg sm:text-2xl font-semibold text-black bg-yellow-400 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:bg-yellow-500 hover:shadow-xl focus:ring-4 focus:ring-yellow-300 focus:outline-none"
         href="https://app.alwayson.ai/login"
         aria-label="Login"
         target="_blank"
-        rel="noreferrer" 
+        rel="noreferrer"
       >
-        Login
+        Login to Platform
       </a>
     </div>
   );
-}
+};
 
 export default Login;
