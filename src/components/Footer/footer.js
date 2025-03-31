3b8e77106b25faa2b0d1b1a9c68446a84746d624
kyle-mucerino <kyle.mucerino@gmail.com>
2025-03-31 08:07:11 -0700

    footer policy

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./footer.css";
+import policy1 from "../../assets/AlwaysOn Acceptable Use Policy.pdf";
+import policy2 from "../../assets/AlwaysOn Cookie Policy.pdf";
+import policy3 from "../../assets/AlwaysOn Privacy Policy.pdf";
+import policy4 from "../../assets/AlwaysOn Terms of Service.pdf";
+
 
 function Footer() {
   return (
@@ -7,16 +12,26 @@ function Footer() {
       <p className="text-2xl">AlwaysOn, Inc. 2025</p>
       <p className="text-xl">Based in Newport Beach, CA</p>
       <div className="flex justify-center mt-4 text-xl">
-        <a href="#top" className="mr-4">
+        <a
+          href={policy1}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4"
+        >
           Privacy Policy
         </a>
-        <a href="#top" className="mr-4">
+        <a
+          href={policy2}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4"
+        >
           Terms of Service
         </a>
-        <a href="#top" className="mr-4">
+        <a href={policy3} target="_blank" rel="noopener noreferrer" className="mr-4">
           Cookie Policy
         </a>
-        <a href="#top">Acceptable Use Policy</a>
+        <a href={policy4} target="_blank" rel="noopener noreferrer">Acceptable Use Policy</a>
       </div>
     </footer>
   );
